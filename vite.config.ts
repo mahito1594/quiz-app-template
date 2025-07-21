01f09479fcaaf0ec728d41c64c1fd6e3ca302d51
Mahito TANNO <mahito@presche.me>
2025-07-21 22:05:05 +0900

    chore(test): setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
+  test: {
+    environment: "happy-dom",
+    setupFiles: "./test/setup.ts",
+    coverage: {
+      include: ["src/**/*"],
+      reporter: ["text", "html", "json-summary", "json"],
+    },
+  },
 });
